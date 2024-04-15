eb47afe11bda170054c165f49965e7ac717a8220
Nife Sofowoke <nifemi.sofowoke@gmail.com>
2024-04-15 20:43:31 +0000

    boto3-additionalcomments

--- test_boto3.py
+++ test_boto3.py
@@ -1,1 +1,8 @@
 import boto3
+
+# how boto3 get access
+# 1) aws configure: default way boto3 gets access to aws, access key ID (gotten in IAM >> security vcredentials)
+# 2) using a IAM role through ec2 instance
+
+# 3) directly by calling a client
+# e.g
