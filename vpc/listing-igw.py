a948a41f5dbe30ca5a45cf1281874b0d13cfa8f0
Nife Sofowoke <nifemi.sofowoke@gmail.com>
2024-04-16 18:56:13 +0000

    list-igw

---
+++ vpc/listing-igw.py
@@ -0,0 +1,12 @@
+# how to list igw with boto3 (describe_internet_gateways)
+
+import boto3
+
+ec2 = boto3.client('ec2')
+
+response = ec2.describe_internet_gateways()
+
+for ig in response["InternetGateways"]:
+    print(ig["InternetGatewayId"])
+    for attachment in ig["Attachments"]:
+        print(attachment["VpcId"], attachment["State"])
